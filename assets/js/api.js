6249aa16b7b83870ef5a9e6353f7da31fb75b9e2
Jérémy Salettes-Wozniak <HarmonyFidelis@gmail.com>
2023-01-13 16:52:02 +0100

    Changement dans l'appel de l'api et de la maniere de faire la pagination

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,40 +1,70 @@
+let main = document.querySelector('main');
 
-// Variables globales
-const leMeilleurFilme = new URL("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score");
-let paginationDesFilms = new URL("http://localhost:8000/api/v1/titles/");
-let paginationDesCategories = new URL("http://localhost:8000/api/v1/genres/");
-let titreDesCategories = [];
-let titreDesFilms = [];
+let meuilleurFilm = async (k=0, j=1) => {
+    // Création de la section MeuilleurFilm
+    let meuilleurFilm = document.querySelector('.meuilleurFilm');
 
-// Fonction qui stocke les pages de l'api dans un tableau pour les filmes
-let stockerPaginationDesFilms = async (urlsArrayApiFilms) => {
-    let response = await fetch(urlsArrayApiFilms);
-    let data = await response.json();
-    titreDesFilms.push(data);
-    if (data.next != null) {
-        console.log(paginationDesFilms);
-        paginationDesFilms = new URL(data.next);
-        stockerPaginationDesFilms(paginationDesFilms);
+    // Création du titre MeuilleurFilm
+    meuilleurFilm.innerHTML = "<h2 class='meuilleurFilm_title meuilleurFilm_title_h2'>Meuilleur film</h2>";
+
+    // Affichage du meilleur film avec appel de l'API
+    try {
+        const response = await fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&page_size=1");
+        const data = await response.json();
+        console.log(data);
+        const bestFilm = document.querySelector('.meuilleurFilm');
+        try {
+            if (data.results[0]) {
+                bestFilm.innerHTML += "<div class='meuilleurFilm_container'></div>";
+                const bestFilmContainer = document.querySelector('.meuilleurFilm_container');
+                
+                bestFilmContainer.innerHTML += "<img class='meuilleurFilm_image' src='" + data.results[0].image_url + "' alt='Image du film " + data.results[0].title + "'>";
+                bestFilmContainer.innerHTML += "<h3 class='meuilleurFilm_title meuilleurFilm_title_h3'>" + data.results[0].title + "</h3>";
+                bestFilmContainer.innerHTML += "<div class='meuilleurFilm_btn'><button type='button'>Voir le film</button></div>";
+        }
+        }
+        catch (error) {
+            console.log(error);
+        }
+    } catch (error) {
+        console.log(error);
     }
-    return titreDesFilms;
 }
-stockerPaginationDesFilms(paginationDesFilms);
+meuilleurFilm();
 
+let LesMeuilleursFilms = async () => {
+    // Création de la section LesMeuilleursFilms
+    let LesMeuilleursFilms = document.querySelector('.LesMeuilleursFilms');
 
-let stockerPaginationDesCategories = async (urlsArrayApiCategories) => {
-    let response = await fetch(urlsArrayApiCategories);
-    let data = await response.json();
-    titreDesCategories.push(data);
-    if (data.next != null) {
-        paginationDesCategories = new URL(data.next);
-        stockerPaginationDesCategories(paginationDesCategories);
+    // Création du titre LesMeuilleursFilms
+    LesMeuilleursFilms.innerHTML = "<h2 class='LesMeuilleursFilms_title LesMeuilleursFilms_title_h2'>Les meuilleurs films</h2>";
+
+    // Affichage des meilleurs films avec appel de l'API
+    try {
+        const response = await fetch("http://localhost:8000/api/v1/titles/?sort_by=-imdb_score&page_size=50");
+        const data = await response.json();
+        console.log(data);
+        if (data.results){
+            LesMeuilleursFilms.innerHTML += "<div class='LesMeuilleursFilms_container'></div>";
+            const LesMeuilleursFilmsContainer = document.querySelector('.LesMeuilleursFilms_container');
+            // PRECEDENTS
+            LesMeuilleursFilmsContainer.innerHTML += "<button class='LesMeuilleursFilms_btn' type='button'><span>Précédents</span></button>";
+            // FILMS
+            LesMeuilleursFilmsContainer.innerHTML += "<div class='LesMeuilleursFilms_container_film'></div>";
+            const LesMeuilleursFilmsContainerFilm = document.querySelectorAll('.LesMeuilleursFilms_container_film');
+
+            for (i = k; i < j; i++) {
+                let film = document.createElement('img');
+                film.setAttribute('class', 'LesMeuilleursFilms_image');
+                film.setAttribute('src', data.results[i].image_url);
+                LesMeuilleursFilmsContainerFilm[0].appendChild(film);
+            }
+            // SUIVANTS
+            LesMeuilleursFilmsContainer.innerHTML += "<button class='LesMeuilleursFilms_btn' type='button'><span>Suivants<span></button>"; 
+        }
+
+    } catch (error) {
+        console.log(error);
     }
-    return titreDesCategories;
 }
-stockerPaginationDesCategories(paginationDesCategories);
-
-
-
-
-console.log(titreDesFilms);
-console.log(paginationDesFilms);
+LesMeuilleursFilms();
